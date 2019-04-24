c427ac53a962ab0cbb9f65abb7e40b80efe3d4f8
Keith Burgie <krburgie@gmail.com>
2019-04-24 00:25:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
-  
+def fizzbuzz(n)
+  if n % 3 == 0
+    if n % 5 == 0
+      "FizzBuzz"
+    end
+    "Fizz"
+  elsif n % 5 == 0
+    "Buzz"
+  else
+    nil 
+  end
 end
